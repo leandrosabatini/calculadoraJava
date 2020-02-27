aadf86a3a4a934045f9e4c60a7651e24d50383bc
Leandro <leandro.sabatini31@gmail.com>
2020-02-27 20:31:53 -0300

    Adicionando seletor de opções e operação de subtração

--- Calculadora.java
+++ Calculadora.java
@@ -2,14 +2,52 @@ import java.util.Scanner;
 
 public class Calculadora {
 	public static void main(String [] args) {
-		Scanner scanner = new Scanner(System.in);
+		start();
+	}
+	
+	public static void start()
+	{
+	    Scanner scanner = new Scanner(System.in);
+	 
+	    System.out.println("Selecione a opção desejada:");
+		System.out.println("0) Sair");
+		System.out.println("1) Soma");
+		System.out.println("2) Subtração");
+		int opcao = scanner.nextInt();
+		
+		int valor1, valor2;
+		
+		switch (opcao) {
+		    case 0:
+		        System.exit(0);
+		    case 1:
+	    
+        		System.out.println("Insira o primeiro valor da soma");
+        	    valor1 = scanner.nextInt();
+        	    
+        	    System.out.println("Insira o segundo valor da soma");
+        	    valor2 = scanner.nextInt();
+        	    
+        	    System.out.println(valor1 + " + " + valor2 + " = " + (valor1 + valor2));
+        	
+	            break;
+	        case 2:
+	            System.out.println("Insira o primeiro valor da subtração");
+        	    valor1 = scanner.nextInt();
+        	    
+        	    System.out.println("Insira o segundo valor da subtração");
+        	    valor2 = scanner.nextInt();
+        	    
+        	    System.out.println(valor1 + " - " + valor2 + " = " + (valor1 - valor2));
+        	    
+                break;
+            default:
+                System.out.println("opção incorreta");
 
-		System.out.println("Insira o primeiro valor da soma");
-	    int valor1 = scanner.nextInt();
-	    
-	    System.out.println("Insira o segundo valor da soma");
-	    int valor2 = scanner.nextInt();
-	    
-	    System.out.println(valor1 + " + " + valor2 + " = " + (valor1 + valor2));
+                break;
+		}
+		
+		start();
 	}
 }
+
